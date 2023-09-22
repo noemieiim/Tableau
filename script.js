6f3c6dccd45bbbc8304d728ce96a321985b3383b
noemieiim <145458232+noemieiim@users.noreply.github.com>
2023-09-22 15:58:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+/**                 Tableau
+ * 
+ * Declare un tableau vide names 
+ * Ajouter "vincent", "Paul", et "Arthur" dans le tableau via la methode push
+ * 
+ * Pour chaque element du tableau
+ * 
+ * Ajouter a la fin "va a la peche"
+ * Afficher chaque element 
+ * 
+ */
+
+let names=[];
+names.push('Vincent',"Paul",'Arthur');
+
+names.forEach(names =>{
+    names += 'va a la peche ';
+    console.log(names)
+})
